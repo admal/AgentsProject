49a0ca89bdfd26ebd07cdece04003ed8ad303ee4
admal <adam.malewski@wp.pl>
2016-05-03 12:31:51 +0200

    register, registerResponse

--- src/main/java/Common/Messages/Register.java
+++ src/main/java/Common/Messages/Register.java
@@ -1,6 +1,7 @@
 package Common.Messages;
 
 import Common.AgentType;
+import Common.IPosition;
 import jade.core.AID;
 
 import java.io.Serializable;
@@ -8,9 +9,10 @@ import java.io.Serializable;
 /**
  * Created by adam on 5/3/16.
  */
-public class Register implements Serializable
+public class Register extends Message
 {
     public AgentType type;
     public AID agentId;
+    public IPosition currPosition;
 }
 
