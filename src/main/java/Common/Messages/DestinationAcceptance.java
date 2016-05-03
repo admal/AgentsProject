441695c6c97101041050f71f0c7b1903384407e5
jedrekf <jedrek@fijalkowscy.com>
2016-05-03 12:40:03 +0200

    master and DestinationMessages updates

--- src/main/java/Common/Messages/DestinationAcceptance.java
+++ src/main/java/Common/Messages/DestinationAcceptance.java
@@ -1,7 +1,14 @@
 package Common.Messages;
 
+import Common.IPosition;
+
 /**
  * Created by jedrek on 03.05.16.
  */
-public class DestinationAcceptance {
+public class DestinationAcceptance extends Message {
+    IPosition clientPosition;
+
+    public DestinationAcceptance(IPosition clientPosition) {
+        this.clientPosition = clientPosition;
+    }
 }
