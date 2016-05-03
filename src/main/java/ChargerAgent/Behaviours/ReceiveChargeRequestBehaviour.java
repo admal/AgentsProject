5508c1c80d050f11ab86b47628e6795b8e0bf21c
Jan Baraniewski <jan@baraniewski.com>
2016-05-03 12:13:29 +0200

    charge request and response

--- src/main/java/ChargerAgent/Behaviours/ReceiveChargeRequestBehaviour.java
+++ src/main/java/ChargerAgent/Behaviours/ReceiveChargeRequestBehaviour.java
@@ -5,7 +5,7 @@ import jade.core.behaviours.CyclicBehaviour;
 /**
  * Created by janbaraniewski on 03/05/16.
  */
-public class ReceiveCarRequestBehaviour extends CyclicBehaviour{
+public class ReceiveChargeRequestBehaviour extends CyclicBehaviour{
 
 
 
