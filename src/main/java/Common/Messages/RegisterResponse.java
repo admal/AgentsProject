c0b552b2dd6a8c7097e6d5fe8e7e2bf5a50ed064
jedrekf <jedrek@fijalkowscy.com>
2016-05-03 14:43:16 +0200

    Register responds with register response with the list of charging stations

--- src/main/java/Common/Messages/RegisterResponse.java
+++ src/main/java/Common/Messages/RegisterResponse.java
@@ -1,9 +1,19 @@
 package Common.Messages;
 
+import Common.AgentClasses.ChargingStation;
+
+import java.util.List;
+
 /**
  * Created by adam on 5/3/16.
  */
 public class RegisterResponse extends Message
 {
     public boolean registered;
+    public List<ChargingStation> chargingStations;
+
+    public RegisterResponse(boolean registered, List<ChargingStation> chargingStations) {
+        this.registered = registered;
+        this.chargingStations = chargingStations;
+    }
 }
