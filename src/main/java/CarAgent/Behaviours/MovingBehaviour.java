8e9311463b2743bd863c42b7e3f1cbd6dfebc241
Jan Baraniewski <janekbaraniewski@gmail.com>
2016-09-02 00:24:45 +0200

    movement

--- src/main/java/CarAgent/Behaviours/MovingBehaviour.java
+++ src/main/java/CarAgent/Behaviours/MovingBehaviour.java
@@ -11,22 +11,13 @@ import jade.core.behaviours.TickerBehaviour;
  */
 public class MovingBehaviour extends TickerBehaviour {
     private CarAgent agent;
-    private int positionIndex;
-    /*
-        2 following vars may not be clear:
-        those are 2 consecutive points from list returned by google maps
-        between which we are currently moving
-    */
-    private int current_starting_id;
-    private int current_ending_id;
-    private IPosition current_position;
+    private int current_subroute_end_id;
     private IPosition step;
     
     public MovingBehaviour(CarAgent agent, long period){
         super(agent,period);
         this.agent = agent;
-        this.current_ending_id = 1;
-        this.current_position = null;
+        this.current_subroute_end_id = 1;
         this.step = null;
     }
     protected void onTick() {
@@ -36,48 +27,48 @@ public class MovingBehaviour extends TickerBehaviour {
     }
 
     private void calculateStep() {
-        IPosition start = this.getAgentPosition(this.current_ending_id - 1);
-        IPosition end = this.getAgentPosition(this.current_ending_id);
+        IPosition start = this.getAgentPosition(this.current_subroute_end_id - 1);
+        IPosition end = this.getAgentPosition(this.current_subroute_end_id);
         float distanceInM = DistanceHelper.fromGeographicToM(start.GetX(),start.GetY(),end.GetX(),end.GetY());
         float step_factor = distanceInM / this.agent.getSpeed();
         float X = (start.GetX() - end.GetX()) / step_factor;
         float Y = (start.GetY() - end.GetY()) / step_factor;
         if(X<0) X*=-1;
         if(Y<0) Y*=-1;
-        this.step = new Position(X,Y);
+        this.step = this.adjustStepToDirection(new Position(X,Y));
+    }
+
+    private IPosition adjustStepToDirection(Position position) {
+        float x = this.getAgentPosition(this.current_subroute_end_id - 1).GetX() < this.getAgentPosition(this.current_subroute_end_id).GetX()?1:-1;
+        float y = this.getAgentPosition(this.current_subroute_end_id - 1).GetY() < this.getAgentPosition(this.current_subroute_end_id).GetY()?1:-1;
+        Position direction = new Position(x,y);
+        return new Position(position.GetX()*direction.GetX(), position.GetY()*direction.GetY());
     }
 
     private void checkIfArrived() {
     }
 
     private void updatePower() {
+
     }
 
     private void moveForward() {
         if(this.checkIfNewSubroute())
             this.changeSubroad();
-        if(positionIndex< agent.getList_route().size()){
-            float kilometers = DistanceHelper.fromGeographicToKm(
-                    agent.getCurrentPosition().GetX(),
-                    agent.getCurrentPosition().GetY(),
-                    agent.getList_route().get(positionIndex).GetX(),
-                    agent.getList_route().get(positionIndex).GetY()
-            );
-            agent.setCurrentPosition(agent.getList_route().get(positionIndex));
-            agent.setChargedPercentage(agent.getChargedPercentage() - (int)kilometers);
-            positionIndex++;
-        }else{
-            agent.stop();
-        }
+        Position newAgentPosition = new Position(
+                this.agent.getCurrentPosition().GetX()+this.step.GetX(),
+                this.agent.getCurrentPosition().GetY()+this.step.GetY()
+        );
+        this.agent.setCurrentPosition(newAgentPosition);
     }
 
     private void changeSubroad() {
-        this.current_ending_id++;
+        this.current_subroute_end_id++;
         this.calculateStep();
     }
 
     private boolean checkIfNewSubroute() {
-        return getAgentPosition(this.current_ending_id-1) == getAgentPosition(this.current_ending_id);
+        return getAgentPosition(this.current_subroute_end_id -1) == getAgentPosition(this.current_subroute_end_id);
     }
 
     private IPosition getAgentPosition(int current_ending_id) {
